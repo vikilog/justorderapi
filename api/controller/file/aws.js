5d709a5a73f63eb90d5d2dd4717bf677fb9136f8
root <root@ip-172-31-39-69.us-east-2.compute.internal>
2021-03-27 13:37:47 +0000

    server updated

--- api/controller/file/aws.js
+++ api/controller/file/aws.js
@@ -4,7 +4,8 @@ const cred=require('../../utility/cred');
 AWS.config.update({
     accessKeyId: cred.aws.ACCESS_KEY_ID,
     secretAccessKey:cred.aws.SECRET_ACCESS_KEY,
-    signatureVersion:'v4'
+    signatureVersion:'v4',
+    region:"us-east-2"
   });
 
   module.exports =new AWS.S3();
